b936d73d087cf32e92e7f37d11eab57ed9750d9f
lucasdkhater <110567682+lucaskhater@users.noreply.github.com>
2025-09-11 12:39:15 +0200

    Implement argument parsing
    Implements a trim and an is_space functions to permit clean parsing
    of arguments in main

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,5 @@
+use crate::cstr::strlen;
+
 const MAX_TOKS: usize = 64;
 
 pub unsafe fn split(mut s: *mut u8, c: u8) -> *const *mut u8 {
@@ -7,20 +9,15 @@ pub unsafe fn split(mut s: *mut u8, c: u8) -> *const *mut u8 {
         return BUF.as_ptr();
     }
 
-    let mut count = 0;
-
-    while *s != 0 && count < MAX_TOKS - 1 {
+    let mut i = 0;
+    while *s != 0 && i < MAX_TOKS - 1 {
         while *s == c {
             *s = 0;
             s = s.add(1);
         }
 
-        if *s == 0 {
-            break;
-        }
-
-        BUF[count] = s;
-        count += 1;
+        BUF[i] = s;
+        i += 1;
 
         while *s != 0 && *s != c {
             s = s.add(1);
@@ -32,7 +29,28 @@ pub unsafe fn split(mut s: *mut u8, c: u8) -> *const *mut u8 {
         }
     }
 
-    BUF[count] = core::ptr::null_mut();
+    BUF[i] = core::ptr::null_mut();
     BUF.as_ptr()
 }
 
+pub unsafe fn trim(mut s: *mut u8) -> *mut u8 {
+    while is_space(*s) {
+        s = s.add(1);
+    }
+
+    if *s == 0 {
+        return s;
+    }
+
+    let mut i = strlen(s) - 1;
+    while is_space(*s.add(i)) {
+        *s.add(i) = 0;
+        i -= 1;
+    }
+    s
+}
+
+#[inline(always)]
+unsafe fn is_space(c: u8) -> bool {
+    matches!(c, b' ' | b'\n' | b'\r' | b'\t' | 0x0B | 0x0C)
+}
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ mod memlibc;
 mod siglibc;
 mod syscalls;
 
-use crate::{cstr::*, syscalls::*};
-use core::mem::zeroed;
+use crate::{cstr::*, lexer::{split, trim}, syscalls::*};
+use core::{mem::zeroed};
 
 const CMD_MAX_LENGTH: usize = 255;
 const P_ALL: i32 = 0;
@@ -17,32 +17,28 @@ const WEXITED: i32 = 4;
 #[no_mangle]
 pub extern "C" fn _main() -> ! {
     let prompt: &[u8] = b"MinSL:$ \0";
-    let mut cmd: [u8; CMD_MAX_LENGTH] = [0; CMD_MAX_LENGTH];
-    let mut count: isize;
  
     loop {
+        let mut cmd: [u8; CMD_MAX_LENGTH] = [0; CMD_MAX_LENGTH];
+        let argv: *const *mut u8;
+
         unsafe {
             _write(
                 1,
                 prompt.as_ptr() as *const u8,
                 strlen(prompt.as_ptr() as *const u8),
             );
-            count = _read(0, cmd.as_mut_ptr().cast(), CMD_MAX_LENGTH);
-        }
+            _read(0, cmd.as_mut_ptr().cast(), CMD_MAX_LENGTH);
 
-        cmd[count as usize - 1] = 0;
+            argv = split(trim(cmd.as_mut_ptr()), b' ');
 
-        unsafe {
             let pid = _fork();
-
             if pid == 0 {
-                let argv: [*const u8; 2] = [cmd.as_ptr() as *const u8, core::ptr::null()];
-
                 let envp: [*const u8; 1] = [core::ptr::null()];
 
                 _execve(
-                    cmd.as_ptr() as *const u8,
-                    argv.as_ptr() as *const *const u8,
+                    *argv as *const u8,
+                    argv as *const *const u8,
                     envp.as_ptr() as *const *const u8,
                 );
                 break;
